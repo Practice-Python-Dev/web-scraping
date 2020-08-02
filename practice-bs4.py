0a1c64fc62f70ff52faf1620a39e56a60790059d
James N <67208442+jamesnorbertowork@users.noreply.github.com>
2020-08-02 19:11:00 +0900

    Update and rename main.py to practice-bs4.py

--- practice-bs4.py
+++ practice-bs4.py
@@ -1,29 +1,37 @@
-#SETUP BEAUTIFUL SOUP
-#'import requests' lets you make http requests using python
+#------------------------------
+# SETUP BEAUTIFUL SOUP
+#------------------------------
+
+# 'import requests' lets you make http requests using python
 import requests
+
 # from (beatiful soupt v4) import package files
 from bs4 import BeautifulSoup
 
 # Setup scraped variable to access elements via the select url
-# Scraped = BeautifulSoup object represents the doc as a nested data structure
 url = "http://books.toscrape.com/index.html"
 response = requests.get(url)
 html = response.content
+# Create scraped - a BeautifulSoup object that represents the html document as a nested data structure
 scraped = BeautifulSoup(html, 'html.parser')
 
+#------------------------------
+# MANIPULATE BEATIFUL SOUP OBJECT
+#------------------------------
 
-
-#CREATE/MAINPULATE BEATIFUL SOUP OBJECT
 # Use the syntax 'scraped.element' (includes html)
 print(scraped.title)
+
 # Use strip() method combined with .text to print just the title content
 print(scraped.title.text.strip())
+
 # Alternatively you could store the title in a variable, then print it
 doc_title = scraped.title.text.strip()
 print(doc_title, '\n\n--------------------')
 
 # Do this with the header tag
 print(scraped.h1.text.strip())
+
 # Do this with the title of the first book
 title = scraped.article.h3.a['title']
 print(title, '\n\n--------------------')
@@ -35,20 +43,23 @@ for link in all_titles:
     print(link['title'])
 print('\n--------------------')
 
-
-
+#------------------------------
 # PRINT ALL PRICES FROM THE PAGE
-# Use srape.select() to grab elements by CSS class selector
+#------------------------------
+
+# Use scrape.select() to grab elements by CSS class selector
 prices = scraped.select(".price_color")
 # Print prices with a loop
 for price in prices:
     print(price)
 print('\n--------------------')
-# Do it without the htmo formatting
+
+# Do it without html formatting
 for price in prices:
      price = float(price.text.lstrip('£'))
      print(price)
 print('\n--------------------')
+
 # Do it in dollars
 for price in prices:
     price = float(price.text.lstrip('£'))
@@ -56,13 +67,14 @@ for price in prices:
     print("$" + price)
 print('\n--------------------')
 
+#------------------------------
+# GRAB TITLES WITH CORRESPONDING PRICES
+#------------------------------
 
-
-#LAST, GRAB TITLES WITH CORRESPONDING PRICES
-# Create list variable to put dictionary inside
-# Grab the articles as raw html data (to manipulate in for loop below)
+# Create a list to put the dictionary inside
 title_prices = []
-# Use srape.select() to grab elements by CSS class selector
+
+# Grab articles as raw html data (to manipulate in for loop) - Grab elements by CSS class selector with scrape.select()
 articles = scraped.select('.product_pod')
 
 # Loop through, extracting the info you need using methods and bs4 syntax
@@ -72,8 +84,6 @@ for article in articles:
     price_float = float(price.text.lstrip('£'))
     # Append each article in articles to our dictionary (put it all together)
     title_prices.append({title: price_float}) # (create the dictinary and append to array)
-#Now we can print the title_prices dictonary (that's been populated)
+
+# Now we can print the title_prices dictonary (that's been populated)
 print(title_prices)
-
-
-
